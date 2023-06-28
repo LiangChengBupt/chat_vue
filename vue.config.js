2f5aee1d4c177d9af760a85ee3615a298854f283
lc <llccheng1026@126.com>
2023-06-28 17:58:29 +0800

    friend_completed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+let proxyObj = {};
+proxyObj['/ws'] = {
+  ws: true,
+  target: "ws://localhost:8082"
+};
+proxyObj['/'] = {
+  ws: false,
+  target: 'http://localhost:8082',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/': ''
+  }
+}
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: proxyObj
+  }
+}
